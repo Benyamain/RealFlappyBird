94f4f3fbe907a0848b05fdbfaa77e1682cf3ab1d
Ben <yacoobbenyamain@gmail.com>
2023-05-06 19:34:25 -0400

    Implemented the additional feature(s) that I mentioned in the last commit. I will work on SFX implementation next! :)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,21 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public enum GameState
+    {
+        Ready,
+        Playing,
+        GameOver
+    }
+
     public Player player;
     private Spawner spawner;
     public Text scoreText;
     public GameObject playButton;
     public GameObject gameOver;
+    public GameObject getReady;
     private int score;
+    private GameState currentState = GameState.Ready;
 
     private void Awake()
     {
@@ -17,7 +26,7 @@ public class GameManager : MonoBehaviour
         player = FindObjectOfType<Player>();
         spawner = FindObjectOfType<Spawner>();
 
-        Pause();
+        SetState(GameState.Ready);
     }
 
     public void Play()
@@ -25,32 +34,56 @@ public class GameManager : MonoBehaviour
         score = 0;
         scoreText.text = score.ToString();
 
-        playButton.SetActive(false);
-        gameOver.SetActive(false);
-
-        Time.timeScale = 1f;
-        player.enabled = true;
-
         Pipes[] pipes = FindObjectsOfType<Pipes>();
 
         for (int i = 0; i < pipes.Length; i++)
         {
             Destroy(pipes[i].gameObject);
         }
+
+        SetState(GameState.Playing);
     }
 
     public void GameOver()
     {
-        playButton.SetActive(true);
-        gameOver.SetActive(true);
-
-        Pause();
+        SetState(GameState.GameOver);
     }
 
     public void Pause()
     {
-        Time.timeScale = 0f;
-        player.enabled = false;
+        SetState(GameState.Ready);
+    }
+
+    private void SetState(GameState newState)
+    {
+        currentState = newState;
+
+        switch (currentState)
+        {
+            case GameState.Ready:
+                Time.timeScale = 0f;
+                player.enabled = false;
+                playButton.SetActive(true);
+                gameOver.SetActive(false);
+                getReady.SetActive(true);
+                break;
+
+            case GameState.Playing:
+                Time.timeScale = 1f;
+                player.enabled = true;
+                playButton.SetActive(false);
+                gameOver.SetActive(false);
+                getReady.SetActive(false);
+                break;
+
+            case GameState.GameOver:
+                Time.timeScale = 0f;
+                player.enabled = false;
+                playButton.SetActive(true);
+                gameOver.SetActive(true);
+                getReady.SetActive(false);
+                break;
+        }
     }
 
     public void IncreaseScore()
@@ -58,5 +91,4 @@ public class GameManager : MonoBehaviour
         score++;
         scoreText.text = score.ToString();
     }
-
 }
